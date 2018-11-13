bb887d5db1708c2cec35d07accfc50a48ca348b1
baileykb <42786867+baileykb@users.noreply.github.com>
2018-11-12 21:41:23 -1000

    Started ClubAdminPage Mockup

---
+++ app/imports/ui/pages/ClubAdminPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MenuBar from '../components/MenuBar';
+import Mid from '../components/Mid';
+
+export default class ClubAdminPage extends React.Component {
+
+  render() {
+    return (
+        <div>
+          <Mid/>
+        </div>
+    );
+  }
+}
