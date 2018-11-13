7cd3719845f5325746170732dfa5073a965d5fa6
Edward Yi <42788217+Edward-Yi@users.noreply.github.com>
2018-11-12 17:18:24 -1000

    tentatively complete

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -9,21 +9,23 @@ class Landing extends React.Component {
           <Grid stackable centered container columns={3}>
 
             <Grid.Column textAlign='center'>
-              <Icon inverted size='huge' name='search'/>
-              <Header as='h1' inverted>Search</Header>
-              <Header as='h3' inverted>Search for specific clubs that you may have heard about. If you do not have a particular club in mind, try searching for one that is within your interest.</Header>
+              <Icon color='green' size='huge' name='search'/>
+              <Header as='h1' color='green'>Search</Header>
+              <Header as='h3' color='green'>Search for specific clubs that you may have heard about. If you do not have a
+                particular club in mind, try searching for one that is within your interest.</Header>
             </Grid.Column>
 
             <Grid.Column textAlign='center'>
-              <Icon inverted size='huge' name='info circle'/>
-              <Header as='h1' inverted>Information</Header>
-              <Header as='h3' inverted>Descriptions of the club are shown to give interested students an overview of the club. Time and location of their meetings are listed for students wanting to join.</Header>
+              <Icon color='green' size='huge' name='info circle'/>
+              <Header as='h1' color='green'>Information</Header>
+              <Header as='h3' color='green'>Descriptions of the club are shown to give interested students an overview of
+                the club. Time and location of their meetings are listed for students wanting to join.</Header>
             </Grid.Column>
 
             <Grid.Column textAlign='center'>
-              <Icon inverted size='huge' name='sitemap'/>
-              <Header as='h1' inverted>Club site</Header>
-              <Header as='h3' inverted>Visit the website of the club to get more detailed information.</Header>
+              <Icon color='green' size='huge' name='sitemap'/>
+              <Header as='h1' color='green'>Club site</Header>
+              <Header as='h3' color='green'>Visit the website of the club to get more detailed information.</Header>
             </Grid.Column>
 
           </Grid>
