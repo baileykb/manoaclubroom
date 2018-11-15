507b5b56eac2dd972b381238b84bb57ce4372c92
Julie Chai <juliechai75@gmail.com>
2018-11-14 14:25:39 -1000

    Cards

--- app/imports/ui/components/Middle.jsx
+++ app/imports/ui/components/Middle.jsx
@@ -1,45 +1,37 @@
 import React from 'react';
-import { Grid, List } from 'semantic-ui-react';
+import { Container, Card } from 'semantic-ui-react';
 
 export default class Middle extends React.Component {
   render() {
     return (
-        <Grid container columns="two">
-          <Grid.Column>
-            <List>
-              <List.Item>Add a club</List.Item>
-              <List.Item/>
-              <List.Item>A</List.Item>
-              <List.Item>Club 1</List.Item>
-              <List.Item>Club 2</List.Item>
-              <List.Item>Club 3</List.Item>
-              <List.Item/>
-              <List.Item>B</List.Item>
-              <List.Item>Club 1</List.Item>
-              <List.Item>Club 2</List.Item>
-              <List.Item>Club 3</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column>
-            <List>
-              <List.Item/>
-              <List.Item/>
-              <List.Item/>
-              <List.Item/>
-              <List.Item/>
-              <List.Item>Edit</List.Item>
-              <List.Item>Edit</List.Item>
-              <List.Item>Edit</List.Item>
-              <List.Item/>
-              <List.Item/>
-              <List.Item/>
-              <List.Item/>
-              <List.Item>Edit</List.Item>
-              <List.Item>Edit</List.Item>
-              <List.Item>Edit</List.Item>
-            </List>
-          </Grid.Column>
-        </Grid>
+        <Container>
+          <Card.Group>
+            <Card>
+              <Card.Content>
+                <Card.Header>Club name</Card.Header>
+                <Card.Description>Club description</Card.Description>
+              </Card.Content>
+            </Card>
+            <Card>
+              <Card.Content>
+                <Card.Header>Club name</Card.Header>
+                <Card.Description>Club description</Card.Description>
+              </Card.Content>
+            </Card>
+            <Card>
+              <Card.Content>
+                <Card.Header>Club name</Card.Header>
+                <Card.Description>Club description</Card.Description>
+              </Card.Content>
+            </Card>
+            <Card>
+              <Card.Content>
+                <Card.Header>Club name</Card.Header>
+                <Card.Description>Club description</Card.Description>
+              </Card.Content>
+            </Card>
+          </Card.Group>
+        </Container>
     );
   }
 }
