3b3955e0bbb2cd4005d45e80973e815bcbf66a16
kristy <gongxue@hawaii.edu>
2018-11-14 20:21:54 -1000

    enhanced for the landing page

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Menu } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const divStyle = { };
     return (
         <footer>
-          <div style={divStyle} className="ui center aligned container">
+          <div style={divStyle} className="ui center aligned container" inverted>
             <hr />
               Department of Information and Computer Sciences <br />
               University of Hawaii<br />
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,29 +6,37 @@ class Landing extends React.Component {
   render() {
     return (
         <div className='landing-background'>
-          <Grid stackable centered container columns={3}>
+          <Grid stackable centered container columns={6} >
+
+            <Grid.Column textAlign='left'>
+              <Icon color=' ' size='huge' name='search'/>
+              <Header as='h1' color='#006400'>Search</Header>
+              <Header as='h3' color='#006400'>
+                Search for specific clubs that you may have heard about.
+                If you do not have a particular club in mind,
+                try searching for one that is within your interest.
+              </Header>
+
+            </Grid.Column>
 
             <Grid.Column textAlign='center'>
-              <Icon color='green' size='huge' name='search'/>
-              <Header as='h1' color='green'>Search</Header>
-              <Header as='h3' color='green'>Search for specific clubs that you may have heard about. If you do not have
-                a
-                particular club in mind, try searching for one that is within your interest.</Header>
             </Grid.Column>
 
               <Grid.Column textAlign='center'>
-                <Icon color='green' size='huge' name='info circle'/>
-                <Header as='h1' color='green'>Information</Header>
-                <Header as='h3' color='green'>Descriptions of the club are shown to give interested students an overview
-                  of
-                  the club. Time and location of their meetings are listed for students wanting to join.</Header>
+
               </Grid.Column>
 
                 <Grid.Column textAlign='center'>
-                  <Icon color='green' size='huge' name='sitemap'/>
-                  <Header as='h1' color='green'>Club site</Header>
-                  <Header as='h3' color='green'>Visit the website of the club to get more detailed information.</Header>
+
                 </Grid.Column>
+
+            <Grid.Column textAlign='center'>
+
+            </Grid.Column>
+
+            <Grid.Column textAlign='center'>
+
+            </Grid.Column>
 
           </Grid>
         </div>
