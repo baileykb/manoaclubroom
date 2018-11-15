61bf842d8fc3660aeb6cce486dfc9a2fc3eafe66
Julie Chai <juliechai75@gmail.com>
2018-11-14 17:41:16 -1000

    Card group

--- app/imports/ui/pages/ManoaClubRoom.jsx
+++ app/imports/ui/pages/ManoaClubRoom.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import TopMenu from '../components/TopMenu';
 import Middle from '../components/Middle';
-import FooterMenu from '../components/FooterMenu';
 
 export default class ManoaClubRoom extends React.Component {
 
