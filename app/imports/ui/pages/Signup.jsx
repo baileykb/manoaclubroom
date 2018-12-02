ad9bb8572be1f79e319ddc4555df75c7a6aa376a
Julie Chai <juliechai75@gmail.com>
2018-12-02 13:26:52 -1000

    Rough version of make profile in signup

--- app/imports/ui/pages/Signup.jsx
+++ app/imports/ui/pages/Signup.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
 import { Accounts } from 'meteor/accounts-base';
+import { Bert } from 'meteor/themeteorchef:bert';
+import { Meteor } from 'meteor/meteor';
+import { Profiles, ProfileSchema } from '/imports/api/profile/profile';
+import TextField from 'uniforms-semantic/TextField';
+import SubmitField from 'uniforms-semantic/SubmitField';
+import ErrorsField from 'uniforms-semantic/ErrorsField';
+import HiddenField from 'uniforms-semantic/HiddenField';
+import AutoForm from 'uniforms-semantic/AutoForm';
 
 /**
  * Signup component is similar to signin component, but we attempt to create a new user instead.
@@ -10,11 +18,13 @@ export default class Signup extends React.Component {
   /** Initialize state fields. */
   constructor(props) {
     super(props);
-    this.state = { email: '', password: '', error: '' };
+    this.state = { email: '', password: '', name: '', major: '', interests: '', error: '' };
     // Ensure that 'this' is bound to this component in these two functions.
     // https://medium.freecodecamp.org/react-binding-patterns-5-approaches-for-handling-this-92c651b5af56
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.insertCallback = this.insertCallback.bind(this);
+    this.formRef = null;
   }
 
   /** Update the form controls each time the user interacts with them. */
@@ -32,6 +42,21 @@ export default class Signup extends React.Component {
         // browserHistory.push('/login');
       }
     });
+  }
+
+  insertCallback(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Add failed: ${error.message}` });
+    } else {
+      Bert.alert({ type: 'success', message: 'Add succeeded' });
+      this.formRef.reset();
+    }
+  }
+
+  submit(data) {
+    const { name, interests, major } = data;
+    const owner = Meteor.user().username;
+    Profiles.insert({ name, interests, major, owner }, this.insertCallback);
   }
 
   /** Display the signup form. */
@@ -63,8 +88,18 @@ export default class Signup extends React.Component {
                       placeholder="Password"
                       onChange={this.handleChange}
                   />
-                  <Form.Button content="Submit"/>
+                  <Form.Button value='Submit' content="Submit"/>
                 </Segment>
+                <AutoForm ref={(ref) => { this.formRef = ref; }} schema={ProfileSchema} onSubmit={this.submit}>
+                  <Segment>
+                    <TextField name='name'/>
+                    <TextField name='interests'/>
+                    <TextField name='major'/>
+                    <SubmitField value='Submit'/>
+                    <ErrorsField/>
+                    <HiddenField name='owner' value='fakeuser@foo.com'/>
+                  </Segment>
+                </AutoForm>
               </Form>
               <Message>
                 Already have an account? Login <Link to="/signin">here</Link>
